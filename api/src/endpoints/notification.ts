5b4ec2658eb62aa5f2f8bb4cc3d7790720ab47ff
Winston Wu <pfunto.wu@gmail.com>
2022-05-07 18:23:35 -0700

    Api console log contains omitted fields

--- api/src/endpoints/notification.ts
+++ api/src/endpoints/notification.ts
@@ -1,14 +1,21 @@
 import { Request, Response } from 'express';
-import axios from 'axios';
 
 export async function createNotification(req: Request, res: Response) {
   try {
-    const { firstName, lastName, supervisor } = req.body;
+    const { firstName, lastName, email, phoneNumber, supervisor } = req.body;
+    const response = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      phoneNumber: phoneNumber,
+      supervisor: supervisor,
+    };
+
     if (!firstName || !lastName || !supervisor) {
       res.status(500).send('Your submission is missing data');
     } else {
-      console.log(req.body);
-      res.send(req.body);
+      console.log(response);
+      res.send(response);
     }
   } catch (e) {
     console.log(e);
